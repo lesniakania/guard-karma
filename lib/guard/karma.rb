052c35472aa255f066c7598293cc32e8eb16f605
Ania Slimak <anna.slimak@lunarlogic.io>
2015-05-26 22:33:45 +0200

    Generated plugin

---
+++ lib/guard/karma.rb
@@ -0,0 +1,7 @@
+require "guard/karma/version"
+
+module Guard
+  module Karma
+    # Your code goes here...
+  end
+end
---
+++ lib/guard/karma/version.rb
@@ -0,0 +1,5 @@
+module Guard
+  module Karma
+    VERSION = "0.0.1"
+  end
+end
